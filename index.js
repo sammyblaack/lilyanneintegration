b9ae7631d987d3a975ffd78380e4fc2f425fa35d
TheDonH <45408894+TheDonH@users.noreply.github.com>
2018-12-02 12:29:41 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const XeroClient = require('xero-node').AccountingAPIClient;
+
+const config = {
+    "appType" : "private",
+    "consumerKey": "K1NA1PB857ZP0FNUAN6HI8OR2VLCWH",
+    "consumerSecret": "FVSQXNLVLBNRYKM25USJTPKF8MHEAO",
+    "privateKeyPath": "privatekey.pem"
+  };
+
+  (async function(){
+    let xero = new XeroClient(config);
+
+    await xero.invoices.create({
+  "Type": "ACCREC",
+  "Contact": { 
+    "ContactID": "dc99a067-03c6-4f12-8fc1-9b8c9a015bde" 
+  },
+  "DueDate": "\/Date(1518685950940+0000)\/",
+  "LineItems": [
+    {
+      "Description": "Services as agreed",
+      "Quantity": "4",
+      "UnitAmount": "100.00",
+      "AccountCode": "200"
+    }
+  ],
+  "Status": "AUTHORISED"     
+    })
+  })();
