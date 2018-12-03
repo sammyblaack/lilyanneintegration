1323a477f71ef564628caec424878a6e72abbba6
TheDonH <45408894+TheDonH@users.noreply.github.com>
2018-12-03 11:32:58 +1000

    Auspost calls now working

--- auspost/base.js
+++ auspost/base.js
@@ -2,17 +2,51 @@ const axios = require('axios');
 const apikey = "ruUybh4YrtotyG6jo6rQZky3ir5ME8Au";
 
 // defining parcel dimension variables
-var frompostcode = "2000";
-var topostcode = "3000";
-var parcel_length = "22";
-var parcel_width = "16";
-var parcel_height = "7.7";
-var parcel_weight = "1.5";
+var frompostcode = '2000';
+var topostcode = '3000';
+var parcel_length = '22';
+var parcel_width = '16';
+var parcel_height = '7.7';
+var parcel_weight = '1.5';
 
-axios({
+async function get_options() {
+  axios({
     method: 'get', 
-    url: 'digitalapi.auspost.com.au/postage/parcel/domestic/size.json HTTP/1.1',
+    url: 'https://digitalapi.auspost.com.au/postage/parcel/domestic/size.json ',
     headers: {
-      'auth-key': 'ruUybh4YrtotyG6jo6rQZky3ir5ME8Au'
+      'AUTH-KEY': 'apikey'
+    }})
+    .then(function(response) {
+      console.log(JSON.stringify(response.data));
+      console.log(response.statusText);
+    })
+    .catch(function(error) {
+      console.log(error);
+    })
+}
+
+async function get_price() {
+  axios({
+    method: 'get', 
+    url: 'https://digitalapi.auspost.com.au/postage/parcel/domestic/calculate.json',
+    headers: {'AUTH-KEY': 'apikey'},
+    params: {
+      'length': parcel_length,
+      'width': parcel_width,
+      'height': parcel_height,
+      'weight': parcel_weight,
+      'from_postcode': frompostcode,
+      'to_postcode': topostcode,
+      'service_code': 'AUS_PARCEL_REGULAR'
     }
-  })
+    })
+    .then(function(response) {
+      console.log(JSON.stringify(response.data));
+      console.log(response.statusText);
+    })
+    .catch(function(error) {
+      console.log(error);
+    })
+  }
+
+
