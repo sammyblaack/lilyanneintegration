615b524a74dd3bcb602172bc958c189b90984749
TheDonH <45408894+TheDonH@users.noreply.github.com>
2018-12-04 11:06:19 +1000

    Added some Auspost functionality+comments

--- auspost/base.js
+++ auspost/base.js
@@ -9,6 +9,7 @@ var parcel_width = '16';
 var parcel_height = '7.7';
 var parcel_weight = '1.5';
 
+//function to get standard parcel box sizes
 async function get_options() {
   axios({
     method: 'get', 
@@ -25,6 +26,31 @@ async function get_options() {
     })
 }
 
+//function to retrieve a list of services+prices
+async function get_services() {
+  axios({
+    method: 'get', 
+    url: 'https://digitalapi.auspost.com.au/postage/parcel/domestic/service.json',
+    headers: {'AUTH-KEY': 'apikey'},
+    params: {
+      'length': parcel_length,
+      'width': parcel_width,
+      'height': parcel_height,
+      'weight': parcel_weight,
+      'from_postcode': frompostcode,
+      'to_postcode': topostcode
+    }
+    })
+    .then(function(response) {
+      console.log(JSON.stringify(response.data));
+      console.log(response.statusText);
+    })
+    .catch(function(error) {
+      console.log(error);
+    })
+  }
+
+//function to get price based on service chosen
 async function get_price() {
   axios({
     method: 'get', 
